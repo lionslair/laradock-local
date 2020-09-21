53c04ce2206f7ebcef40bc6bb5dfffe0bc7fdb54
Nathan Rzepecki <nathan@lionslair.net.au>
2020-09-21 13:58:37 +0800

    Update restart to stop and start

--- restart.sh
+++ restart.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-cd ~/Code/laradock; docker-compose restart caddy mysql adminer redis elasticsearch kibana laravel-horizon selenium workspace php-worker
+#cd ~/Code/laradock; docker-compose restart caddy mysql adminer redis elasticsearch kibana laravel-horizon selenium workspace php-worker
+cd ~/Code/laradock; ./down.sh
+cd ~/Code/laradock; ./up.sh
