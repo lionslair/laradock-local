068571455f7848a8871116116eb2642a5d82b0ce
Nathan Rzepecki <nathan@lionslair.net.au>
2021-07-13 08:50:17 +0800

    Add melbourneacl to env

--- mysql/docker-entrypoint-initdb.d/createdb.sql
+++ mysql/docker-entrypoint-initdb.d/createdb.sql
@@ -226,4 +226,10 @@ GRANT ALL ON `xconnect`.* TO 'homestead'@'%' ;
 CREATE DATABASE IF NOT EXISTS `xconnect_testing` COLLATE 'utf8_general_ci' ;
 GRANT ALL ON `xconnect_testing`.* TO 'homestead'@'%' ;
 
+CREATE DATABASE IF NOT EXISTS `melbourneacl` COLLATE 'utf8_general_ci' ;
+GRANT ALL ON `melbourneacl`.* TO 'homestead'@'%' ;
+
+CREATE DATABASE IF NOT EXISTS `melbourneacltesting` COLLATE 'utf8_general_ci' ;
+GRANT ALL ON `melbourneacl_testing`.* TO 'homestead'@'%' ;
+
 FLUSH PRIVILEGES ;
