65276dd946567c609172e396f5feb7ca47d63d38
Nathan Rzepecki <nathan@lionslair.net.au>
2021-09-19 14:44:37 +0800

    Got php 7.4 and 8.0 working with caddy

--- up.sh
+++ up.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #cd ~/Code/laradock; docker-compose up -d caddy mysql adminer redis elasticsearch kibana laravel-horizon selenium workspace php-worker
-cd ~/Code/laradock; docker-compose up -d caddy mysql adminer redis laravel-horizon selenium workspace workspace-legacy php-worker mailhog minio meilisearch
+cd ~/Code/laradock; docker-compose up -d caddy mysql adminer redis laravel-horizon selenium workspace php-worker mailhog minio meilisearch
