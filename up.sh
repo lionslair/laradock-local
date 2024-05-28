662afe7142c73897fad8da3edc27bee93d3a5edb
Nathan Rzepecki <nathan@lionslair.net.au>
2024-05-28 14:26:04 +0800

    Do not explictily need it

--- up.sh
+++ up.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #cd ~/Code/laradock; docker compose up -d caddy mysql adminer redis elasticsearch kibana laravel-horizon selenium workspace php-worker
-cd ~/Code/laradock; docker compose up -d caddy mysql adminer redis laravel-horizon selenium workspace php-worker mailhog minio meilisearch typesense puppeteer
+cd ~/Code/laradock; docker compose up -d caddy mysql adminer redis laravel-horizon selenium workspace php-worker mailhog minio meilisearch typesense
