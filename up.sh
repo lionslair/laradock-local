719201723da45c47f63098f3691cc7ee8337acf3
Nathan Rzepecki <nathan@lionslair.net.au>
2025-08-25 17:24:51 +0800

    Add minio back

--- up.sh
+++ up.sh
@@ -3,4 +3,4 @@
 #cd ~/Code/laradock; docker compose up -d caddy mysql adminer redis elasticsearch kibana laravel-horizon selenium workspace php-worker
 
 # disabled minio selenium php-fpm-7.4
-cd ~/Code/laradock; docker compose up -d caddy mysql adminer redis laravel-horizon workspace php-worker mailhog typesense php-fpm
+cd ~/Code/laradock; docker compose up -d caddy mysql adminer redis laravel-horizon workspace php-worker mailhog typesense php-fpm minio
