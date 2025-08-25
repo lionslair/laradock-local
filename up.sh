e36ced69cd7c8d99b1beb32fd9a1fbfa2702b3d3
Nathan Rzepecki <nathan@lionslair.net.au>
2025-08-25 16:35:26 +0800

    got xdebug working

--- up.sh
+++ up.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 #cd ~/Code/laradock; docker compose up -d caddy mysql adminer redis elasticsearch kibana laravel-horizon selenium workspace php-worker
-#cd ~/Code/laradock; docker compose up -d caddy mysql adminer redis laravel-horizon selenium workspace php-worker mailhog minio typesense php-fpm php-fpm-7.4
-cd ~/Code/laradock; docker compose up -d caddy mysql adminer redis laravel-horizon selenium workspace php-worker mailhog minio typesense php-fpm
+
+# disabled minio selenium php-fpm-7.4
+cd ~/Code/laradock; docker compose up -d caddy mysql adminer redis laravel-horizon workspace php-worker mailhog typesense php-fpm
